54d2502845dece8a3b1b4e23bd52cbadb0dae2e0
Jenny Li <jennyli@dyn-209-2-234-156.dyn.columbia.edu>
2018-02-17 20:56:40 -0500

    2nd

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-from flask import Flask, render_templateapp = Flask(__name__)@app.route('/')def base():    return render_template("base.html")if __name__ == "__main__":	app.run(host="0.0.0.0")
+from flask import Flask, render_templateapp = Flask(__name__)@app.route('/')def base():    return render_template("base.html")@app.route('/results')def results():    return render_template("results.html")if __name__ == "__main__":	app.run(host="0.0.0.0")
